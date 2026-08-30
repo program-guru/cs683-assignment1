15f486b44a9dcd59ddeee504749a96b08ccf2048
Apollo-Blaze <srichandsureshrocks@gmail.com>
2026-08-30 09:48:44 +0530

    feat: tiling

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -5,5 +5,31 @@
 void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     // TODO(student): replace this placeholder with your tiled/blocked implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;
+
+    const int TILE = 16;
+    // Increments by the size of the tile in output
+    for (int oy0 = 0; oy0 < H; oy0 += TILE) {
+        for (int ox0 = 0; ox0 < W; ox0 += TILE) {
+            int oy_max = oy0 + TILE;
+            if (oy_max > H)
+                oy_max = H;
+            int ox_max = ox0 + TILE;
+            if (ox_max > W)
+                ox_max = W;
+            // Same as Naive convolution
+            for (int oy = oy0; oy < oy_max; ++oy) {
+                for (int ox = ox0; ox < ox_max; ++ox) {
+                    float acc = 0.0f;
+                    for (int ky = 0; ky < K; ++ky) {
+                        for (int kx = 0; kx < K; ++kx) {
+                            acc +=in[(oy + ky) * in_stride + (ox + kx)]*ker[ky * K + kx];
+                        }
+                    }
+                    out[oy * W + ox] = acc;
+                }
+            }
+        }
+    }
 }
