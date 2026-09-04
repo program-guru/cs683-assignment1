86c2b153ffab3d716e94c7fee219eb1f751dadee
program-guru <shivam.suyash@gmail.com>
2026-09-04 14:45:08 +0530

    feat: reorder
    removed separate intiliazation

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -8,17 +8,13 @@ void conv_reorder(const float* in, float* out, const float* ker,
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
 
-    for(int oy = 0; oy < H; ++oy) {
-        for(int ox = 0; ox < W; ++ox) {
-            out[oy * W + ox] = 0.0f;
-        }
-    }
-
-    for(int ky = 0; ky < K; ++ky) {
-        for(int kx = 0; kx < K; ++kx) {
-            for(int oy = 0; oy < H; ++oy) {
-                for(int ox = 0; ox < W; ++ox) {
-                    out[oy * W + ox] += in[(oy + ky) * in_stride + (ox + kx)] * ker[ky * K + kx];
+    for (int ky = 0; ky < K; ++ky) {
+        for (int kx = 0; kx < K; ++kx) {
+            const float w = ker[ky * K + kx];   
+            for (int oy = 0; oy < H; ++oy) {
+                for (int ox = 0; ox < W; ++ox) {
+                    if(ky == 0 && kx == 0) out[oy * W + ox] = 0.0f; // Initialize output for the first kernel element
+                    out[oy * W + ox] += w * in[(oy + ky) * in_stride + (ox + kx)];
                 }
             }
         }
