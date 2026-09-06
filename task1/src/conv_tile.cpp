aea94bd2846377632257c214c5983ec928e6972d
program-guru <shivam.suyash@gmail.com>
2026-09-06 15:27:16 +0530

    chore: change tile size

--- task1/src/conv_tile.cpp
+++ task1/src/conv_tile.cpp
@@ -4,20 +4,17 @@
 
 void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
-    // TODO(student): replace this placeholder with your tiled/blocked implementation.
     const int p = K / 2;
     const int in_stride = W + 2 * p;
+    const int tile_size = 32;
 
-    const int TILE = 16;
     // Increments by the size of the tile in output
-    for (int oy0 = 0; oy0 < H; oy0 += TILE) {
-        for (int ox0 = 0; ox0 < W; ox0 += TILE) {
-            int oy_max = oy0 + TILE;
-            if (oy_max > H)
-                oy_max = H;
-            int ox_max = ox0 + TILE;
-            if (ox_max > W)
-                ox_max = W;
+    for (int oy0 = 0; oy0 < H; oy0 += tile_size) {
+        for (int ox0 = 0; ox0 < W; ox0 += tile_size) {
+            
+            int oy_max = oy0 + tile_size > H ? H : oy0 + tile_size;
+            int ox_max = ox0 + tile_size > W ? W : ox0 + tile_size;
+            
             // Same as Naive convolution
             for (int oy = oy0; oy < oy_max; ++oy) {
                 for (int ox = ox0; ox < ox_max; ++ox) {
